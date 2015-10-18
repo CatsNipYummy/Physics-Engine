11af9b028f555e7dff02ad80f3f48158e777f704
Anil Unnikrishnan <unnikrishnan.anil@gmail.com>
2015-10-17 23:30:59 -0400

    Vector Functions 1. Multiplication overloading 2. Addition overloading 3. Subtraction overloading

--- include/bowthika/core.h
+++ include/bowthika/core.h
@@ -42,7 +42,56 @@ namespace bowthika {
 
 		// Turns this vector into a vector of unit length
 		void normalize() {
-
+            real magnitudeValue = magnitude();
+            if (magnitudeValue > 0) {
+                (*this) *= ((real)1)/magnitudeValue;
+            }
 		}
+        
+        /* Operator overload to allow scalar multiplication across the vector
+           aV = a*V (a being the scalar)
+         */
+        
+        void operator*=(const real value) {
+            x *= value;
+            y *= value;
+            z *= value;
+        }
+        
+        // Returns the copy of the scaled vector value
+        Vector3 operator*(const real value) const {
+            return Vector3(x*value, y*value, z*value);
+        }
+        
+        // Vector3 Addition
+        void operator += (const Vector3& vector) {
+            x += vector.x;
+            y += vector.y;
+            z += vector.z;
+        }
+        
+        // Returns the value of the given vector added to this vector
+        Vector3 operator + (const Vector3& vector) const {
+            return Vector3(x + vector.x, y + vector.y, z + vector.z);
+        }
+        
+        // Vector3 Subtraction
+        void operator -= (const Vector3& vector){
+            x -= vector.x;
+            y -= vector.y;
+            z -= vector.z;
+        }
+        
+        // Returns the value of the given vector subtracted from this vector
+        Vector3 operator - (const Vector3& vector) const {
+            return Vector3(x - vector.x, y - vector.y, z - vector.z);
+        }
+        
+        // Adds the given vector scaled by the given about to this vector (A + cB)
+        void addScaledVector(const Vector3& vector, real scale) {
+            x += scale * vector.x;
+            y += scale * vector.y;
+            z += scale * vector.z;
+        }
 	};
 }
